50d7c4b1cceee77690fa6b68286c56f3b4c68531
Sergey <email@example.ruseregpets.ru@mail.ru>
2023-10-16 19:31:56 +0300

    add project

---
+++ project/simpleapp/templatetags/custom_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+CURRENCIES_SIMBOLS = {
+    'rub': '₽',
+    'usd': '$'
+}
+
+@register.filter()
+
+def currency(value, code='rub'):
+    """
+    :param value: значение, к которому нужно применить фильтр
+    """
+    postfix = CURRENCIES_SIMBOLS[code]
+    return f'{value} {postfix}'
+
+# @register.filter()
+
+
+
