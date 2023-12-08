4aef7bb30314ce22075fa7a821a421935c2e8a72
Sergey <email@example.ruseregpets.ru@mail.ru>
2023-12-08 12:48:44 +0300

    D6.1 Отправка письм c HTML

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
 from allauth.account.forms import SignupForm
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 
 
 class SignUpForm(UserCreationForm):
@@ -28,12 +28,17 @@ class CustomSignupForm(SignupForm):
         user = super().save(request)
         common_users = Group.objects.get(name='common users')
         user.groups.add(common_users)
-        send_mail(
-            subject='Добро пожааловать в наш интернет-магазин!',
-            message=f'{user.username}, вы успешно зарегестрировались!',
-            from_email=None, # будет использовано значение DEFAULT_FROM_EMAIL
-            recipient_list=[user.email],
+        subject = 'Добро пожаловать в наш интернет-магазин!',
+        text = f'{user.username}, вы успешно зарегестрировались на сайте!'
+        html = (
+            f'<b>{user.username}</b>, вы успешно зарегестрировались на '
+            f'<a href="http://127.0.0.1:8000/products">сайте</a>!'
         )
+        msg = EmailMultiAlternatives(
+            subject=subject, body=text, from_email=None, to=[user.email]
+        )
+        msg.attach_alternative(html, 'text/html')
+        msg.send()
         return user
 
 
